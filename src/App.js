d2e75e974d60d563efa894d596c6e7bf7e1212ab
modis1234 <38834644+modis1234@users.noreply.github.com>
2020-11-30 10:29:43 +0900

    11/30 생활코딩 React-컴포넌트 제작

--- src/App.js
+++ src/App.js
@@ -1,25 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import TOC from'./components/TOC';
+import Content from'./components/Content';
+import Subject from'./components/Subject';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// function App() {
+//   return (
+//     <div className="App">
+//       Hello, React!!
+//     </div>
+//   );
+// }
+
+
+// class Subject extends Component {
+//   render() {
+//     return (
+//       <header>
+//         <h1>{this.props.title}</h1>
+//         {this.props.sub}
+//       </header>
+//     );
+//   }
+// }
+
+// @desc State 사용자가 알 필요도 없고 알아서는 안되는 컴포넌트에 내부에 정보
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <Subject title="WEB" sub="world wide web"></Subject>
+        <TOC></TOC>
+        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+      </div>
+    );
+  }
 }
+
 
 export default App;
---
+++ src/components/TOC.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+
+
+class TOC extends Component {
+    render() {
+      return (
+        <nav>
+          <ul>
+            <li><a href="1.html" id="">HTML</a></li>
+            <li><a href="2.html" id="">CSS</a></li>
+            <li><a href="3.html" id="">JavaScript</a></li>
+          </ul>
+        </nav>
+      );
+    }
+  }
+
+  export default TOC;
