1e832615da26997fa4d716522136682d1e531da6
modis1234 <38834644+modis1234@users.noreply.github.com>
2020-12-01 16:35:58 +0900

    12/01 생활코딩 React-이벤트

--- src/App.js
+++ src/App.js
@@ -11,26 +11,60 @@ class App extends Component {
     // component의 render가 동작하기 전에 constructor가 먼저 동작해 컴포넌트를 초기화한다.
     super(props);
     this.state = {
+      mode: 'read',
+      selected_content_id:2,
       subject: { title: "WEB", sub: "World Wide Web!" },
-      contents:[
-        {id:1, title:'HTML', desc:'HTML is HyperText...'},
-        {id:2, title:'CSS', desc:'CSS is for design'},
-        {id:3, title:'JavaScript', desc:'JavaScript is for interactive'}
+      welcome: { title: 'welcome', desc: 'Hello, React!!' },
+      contents: [
+        { id: 1, title: 'HTML', desc: 'HTML is HyperText...' },
+        { id: 2, title: 'CSS', desc: 'CSS is for design' },
+        { id: 3, title: 'JavaScript', desc: 'JavaScript is for interactive' }
       ]
     }
 
   }
 
   render() {
+    console.log('App render')
+    var _title, _desc = null;
+    if (this.state.mode === 'welcome') {
+      _title = this.state.welcome.title;
+      _desc = this.state.welcome.desc;
+    }
+    else if (this.state.mode === 'read') {
+      var i = 0;
+      while(i < this.state.contents.length){
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
+    }
+    console.log('render:', this)
     return (
       <div className="App">
-        <Subject 
-          title={this.state.subject.title} 
-          sub={this.state.subject.sub}>
+        <Subject
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          onChangePage={function () {
+            this.setState({ mode: 'welcome' });
+          }.bind(this)}
+        >
         </Subject>
         {/* TOC Component에 this.state.contents 데이터를 전달 */}
-        <TOC data={this.state.contents}></TOC>
-        <Content title="HTML" desc="HTML is HyperText Markup Language."></Content>
+        <TOC 
+          onChangePage={function (id) {
+            this.setState({
+              mode:'read',
+              selected_content_id:Number(id)
+            })
+          }.bind(this)} 
+          data={this.state.contents}
+        ></TOC>
+        <Content title={_title} desc={_desc}></Content>
       </div>
     );
   }
